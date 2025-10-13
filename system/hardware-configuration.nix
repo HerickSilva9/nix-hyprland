d91d844eb3362f4bc24f1727e1d0eec72fa16a12
hericksilva9 <hericksilva9@outlook.com>
2025-10-12 21:21:27 -0300

    atualização do hardware-configuration devido nstalação em um novo hardware

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -14,12 +14,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6cc93ef7-03e3-4001-affb-ba733c752076";
+    { device = "/dev/disk/by-uuid/e53521b1-8894-47c6-99c1-7d12fd7f59eb";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1584-1252";
+    { device = "/dev/disk/by-uuid/6938-B774";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
@@ -31,6 +31,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0f0u9.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp8s0f3u2.useDHCP = lib.mkDefault true;
 
