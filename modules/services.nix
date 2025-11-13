5d64dff157218b46e13135574c6a2ec348a2618f
HerickSilva9 <hericksilva9@outlook.com>
2025-11-13 00:53:25 -0300

    Remove serviço duplicado

--- modules/services.nix
+++ modules/services.nix
@@ -3,7 +3,6 @@
 {
 
   services.gnome.gnome-keyring.enable = true;
-  services.pipewire.enable = true;
 
   # Habilita o udisks2 para gerenciamento de dispositivos
   services.udisks2.enable = true;
